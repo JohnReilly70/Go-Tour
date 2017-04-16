c460aab1d0f76c0eedcad821075bc454d88c890a
SgtSafety <johnreilly1989@hotmail.co.uk>
2017-04-16 16:25:42 +0100

    Adding Completed Files from Go Tour

---
+++ sandbox.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func main(){
+
+  fmt.Println("Welcome to the playground")
+
+  fmt.Println("The time is", time.Now(),"at the beep")
+
+  fmt.Println("BEEEEEEEEEEEEEEEPPPPPPPP")
+}
