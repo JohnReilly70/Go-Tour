b65e714896775e9dfa689a46ed85f254abe9c7ab
SgtSafety <johnreilly1989@hotmail.co.uk>
2017-04-17 21:58:17 +0100

    Completed up to type-conversion

---
+++ short-variable-declartions.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var test int = 1
+// testing := 100 - CANNOT USE SHORT HAND OUTSIDE A FUNCTION
+
+func main() {
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(testing,test,i, j, k, c, python, java)
+}
