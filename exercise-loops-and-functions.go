2dfd25b5183d3b4fb34e85cb2be3a585aac8d71c
SgtSafety <johnreilly1989@hotmail.co.uk>
2017-05-01 18:49:43 +0100

    Finished Square Root Exercise/lets pretty it up later

---
+++ exercise-loops-and-functions.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Sqrt_1_eq(answer, x float64) float64 {
+	return answer*answer - x
+}
+func Sqrt_2_eq(answer float64) float64 {
+	return 2 * answer
+}
+
+func Sqrt(x float64) float64 {
+	var answer float64 = x / 2
+	var func1, func2 float64 = 0, 0
+	for i := 0; i < 10; i++ {
+		//fmt.Println(Sqrt_1_eq(answer, x))
+		func1 = Sqrt_1_eq(answer, x)
+		//fmt.Println(func1)
+		func2 = Sqrt_2_eq(answer)
+		//fmt.Println(func2)
+		answer = answer - (func1 / func2)
+		//fmt.Println(answer)
+	}
+	return answer
+}
+
+func main() {
+	fmt.Printf("Square root of 9: %v\n", Sqrt(9))
+	fmt.Printf("Square root of 12: %v\n", Sqrt(12))
+}
